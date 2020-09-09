2780c8c03e4f08e102fb9257abaf17a34feb9a53
Michal_Marszalek <1aam2am1@gmail.com>
2020-09-09 10:33:45 +0200

    Added tests target.

---
+++ tests/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Michal_Marszalek on 08.09.2020.
+//
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+int main(int argc, char* argv[]){
+    return Catch::Session().run(argc, argv);
+}
