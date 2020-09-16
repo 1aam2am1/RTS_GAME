8256f03c980ae939f77d07c7960eeb19deb795b2
Michal_Marszalek <1aam2am1@gmail.com>
2020-09-16 19:22:29 +0200

    Added Behavior header.

--- GameClient/include/GameClient/Unity/Core/Behaviour.h
+++ GameClient/include/GameClient/Unity/Core/Behaviour.h
@@ -10,7 +10,16 @@
 
 /// Behaviours are Components that can be enabled or disabled.
 class Behaviour : public Component {
+public:
 
+    /// Enabled Behaviours are Updated, disabled Behaviours are not.
+    bool enabled;
+
+    /// Has the Behaviour had active and enabled called?
+    /// \note A GameObject can be active or not active. Similarly, a script can be enabled or disabled.
+    /// If a GameObject is active and has an enabled script then isActiveAndEnabled will return true.
+    /// Otherwise false is returned.
+    bool isActiveAndEnabled;
 };
 
 
