7ea526890749c68ea571168418a6d203b138da9b
Michal_Marszalek <1aam2am1@gmail.com>
2020-09-16 19:18:43 +0200

    Added Component header.

--- GameClient/include/GameClient/Unity/Core/Component.h
+++ GameClient/include/GameClient/Unity/Core/Component.h
@@ -7,10 +7,68 @@
 
 
 #include "Object.h"
+#include <GameClient/TPtr.h>
+
+class GameObject;
+
+class Transform;
 
 /// Base class for everything attached to GameObjects.
 class Component : public Object {
+public:
 
+    /// The game object this component is attached to. A component is always attached to a game object.
+    ///TPtr<GameObject> gameObject; TODO: make it field;
+
+    /// The tag of this game object.
+    std::string tag;
+
+    /// The Transform attached to this GameObject.
+    ///TPtr<Transform> transform; TODO: make it field;
+
+    /// Is this game object tagged with tag ?
+    /// \param tag The tag to compare.
+    /// \return Returns true if the same
+    bool CompareTag(std::string_view tag) const noexcept;
+
+    /// Returns the component of Type type if the game object has one attached, null if it doesn't.
+    /// \tparam T The type of Component to retrieve.
+    /// \return Component A component of the matching type, if found.
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>, int> = 0>
+    TPtr<T> GetComponent() const;
+
+    /// Returns the component of Type type in the GameObject or any of its children using depth first search.
+    /// \note A component is returned only if it is found on an active GameObject.
+    /// \tparam T The type of Component to retrieve.
+    /// \return Component A component of the matching type, if found.
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>, int> = 0>
+    TPtr<T> GetComponentInChildren() const;
+
+    /// Retrieves the component of Type type in the GameObject or any of its parents.
+    /// \tparam T Type of component to find.
+    /// \return Component Returns a component if a component matching the type is found. Returns null otherwise.
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>, int> = 0>
+    TPtr<T> GetComponentInParent() const;
+
+    /// Returns all components of Type type in the GameObject.
+    /// \tparam T The type of component to retrieve.
+    /// \return Array of components
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>, int> = 0>
+    std::vector<TPtr<T>> GetComponents() const;
+
+    /// Returns all components of Type type in the GameObject or any of its children.
+    /// \tparam T The type of Component to retrieve.
+    /// \param includeInactive Should Components on inactive GameObjects be included in the found set?
+    /// \return Array of components
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>, int> = 0>
+    std::vector<TPtr<T>> GetComponentsInChildren(bool includeInactive = false) const;
+
+    /// Returns all components of Type type in the GameObject or any of its parents.
+    /// \tparam T The type of Component to retrieve.
+    /// \param includeInactive Should inactive Components be included in the found set?
+    /// \return
+    template<typename T, std::enable_if_t<std::is_base_of_v<Component, T>, int> = 0>
+    std::vector<TPtr<T>> GetComponentsInParent(bool includeInactive = false) const;
 };
 
 
