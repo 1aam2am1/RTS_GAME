d419b02f290472cc4e3c8f65d95b2723e399f5e7
Michal_Marszalek <1aam2am1@gmail.com>
2020-10-08 13:28:57 +0200

    Added my own YAML Parser

---
+++ GameApi/include/Yaml/Node/Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michal_Marszalek on 08.10.2020.
+//
+
+#include "Node.h"
---
+++ GameApi/include/Yaml/Node/Node.h
@@ -0,0 +1,14 @@
+//
+// Created by Michal_Marszalek on 08.10.2020.
+//
+
+#ifndef RTS_GAME_NODE_H
+#define RTS_GAME_NODE_H
+
+
+class Node {
+
+};
+
+
+#endif //RTS_GAME_NODE_H
