d725403f75b9e3f6d84b184606d0bb31687520c4
Michal_Marszalek <1aam2am1@gmail.com>
2020-10-15 18:16:18 +0200

    Added Selection for knowing what is selected in inspector or project window.

---
+++ GameClient/include/GameClient/Unity/Editor/Selection.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Michal_Marszalek on 15.10.2020.
+//
+
+#include "Selection.h"
+
+TPtr<GameObject> Selection::activeGameObject{nullptr};
+
+TPtr<Object> Selection::activeObject{nullptr};
+
+TPtr<Transform> Selection::activeTransform{nullptr};
---
+++ GameClient/include/GameClient/Unity/Editor/Selection.h
@@ -0,0 +1,26 @@
+//
+// Created by Michal_Marszalek on 15.10.2020.
+//
+
+#ifndef RTS_GAME_SELECTION_H
+#define RTS_GAME_SELECTION_H
+
+#include <GameClient/Unity/Core/GameObject.h>
+#include <GameClient/TPtr.h>
+
+class Selection {
+public:
+    /// Returns the active game object. (The one shown in the inspector).
+    /// \details It will also return game objects that might be Prefabs or non-modifiable objects.
+    static TPtr<GameObject> activeGameObject;
+
+    /// Returns the actual object selection. Includes Prefabs, non-modifiable objects.
+    static TPtr<Object> activeObject;
+
+    /// Returns the active transform. (The one shown in the inspector).
+    /// \details This will never return Prefabs or non-modifiyable objects.
+    static TPtr<Transform> activeTransform;
+};
+
+
+#endif //RTS_GAME_SELECTION_H
